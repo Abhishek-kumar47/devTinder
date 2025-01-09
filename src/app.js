3cb43fd18938f19cb8ffafb9054175e2326a17de
Abhishek-kumar47 <kumar47abhishek1802@gmail.com>
2025-01-09 14:52:51 +0530

    best practice to connect to the database and starting a server

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express= require('express');
-require("./config/database");
+const connectDB = require("./config/database");
 
 const app= express();
-
+connectDB()
+    .then(() => {
+        console.log("MongoDB Connected...");
+        
 app.listen(7777, () => {
     console.log("Server is running on port 7777");
 });
+    })
+    .catch(err => {
+        console.error("MongoDB Connection Error:", err)
+    });
--- src/config/database.js
+++ src/config/database.js
@@ -4,11 +4,5 @@ const mongoose = require('mongoose');
 const connectDB = async() =>{
    await mongoose.connect("mongodb+srv://Abhishek:DjafTdrkRi3Oehow@hellonode.0kn5t.mongodb.net/devTinder");
 };
+module.exports = connectDB;
 
-connectDB()
-    .then(() => {
-        console.log("MongoDB Connected...")
-    })
-    .catch(err => {
-        console.error("MongoDB Connection Error:", err)
-    });
