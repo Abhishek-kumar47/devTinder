b7770dd49c8ceeb8c9100ab885c021d9da2ca7aa
Abhishek-kumar47 <kumar47abhishek1802@gmail.com>
2025-01-09 15:52:49 +0530

    initializing the database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 const express= require('express');
 const connectDB = require("./config/database");
+const User = require("./models/user");
+
 
 const app= express();
+
+app.post("/signup",async (req,res) =>{
+    //creating a new instance of the user model
+    const user = new User({
+        firstName:"Abhishek",
+        lastName:"kumar",
+        emailId:"abhi@gmail.com",
+        password:"123456",
+    });
+  try { 
+     await user.save();
+    res.send("User added successfully");
+} catch(err){
+    res.status(400).res("error saving the user" + err.message);
+}
+});
+
 connectDB()
     .then(() => {
         console.log("MongoDB Connected...");
