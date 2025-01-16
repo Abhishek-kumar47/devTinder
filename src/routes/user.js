478fd2b7bcd52933e037a19db3ee780a84eb42c0
Abhishek-kumar47 <kumar47abhishek1802@gmail.com>
2025-01-17 02:02:53 +0530

    pagination in feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -55,6 +55,11 @@ userRouter.get("/user/feed", userAuth,async (req,res)=>{
     try{
         const loggedInUser = req.user;
 
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit>50 ? 50: limit;
+        const skip = (page - 1) * limit;
+
         const connectionRequests = await ConnectionRequest.find({
             $or:[
                 {fromUserId: loggedInUser._id},
@@ -72,7 +77,10 @@ userRouter.get("/user/feed", userAuth,async (req,res)=>{
                 {_id:{$nin : Array.from(hideUsersFromFeed)}},
                 {_id:{$ne : loggedInUser._id}}
             ],
-        }).select(USER_SAFE_DATA);
+        })
+        .select(USER_SAFE_DATA)
+        .skip(skip)
+        .limit(limit);
         res.send(users);
     }catch(err){
         res.status(400).json({error: err.message});
