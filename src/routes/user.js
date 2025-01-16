7fbc1eb8a3ea6fa57a508cf8f863d436e1663684
Abhishek-kumar47 <kumar47abhishek1802@gmail.com>
2025-01-17 01:34:47 +0530

    Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRouter= express.Router();
 const {userAuth} = require("../middlewares/auth");
 const ConnectionRequest= require("../models/connectionRequest");
+const User= require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
@@ -50,4 +51,31 @@ userRouter.get("/user/connections", userAuth, async (req,res) =>{
     }
 });
 
+userRouter.get("/user/feed", userAuth,async (req,res)=>{
+    try{
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or:[
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach(req => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+        const users = await User.find({
+            $and :[
+                {_id:{$nin : Array.from(hideUsersFromFeed)}},
+                {_id:{$ne : loggedInUser._id}}
+            ],
+        }).select(USER_SAFE_DATA);
+        res.send(users);
+    }catch(err){
+        res.status(400).json({error: err.message});
+    }
+})
 module.exports = userRouter;
